2962415461b6de4480495410d4f8ba8d706659f2
Daniil Veremchuk <daniel@verydanny.com>
2019-09-20 16:00:57 -0400

    init

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: '2018',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    'inferno',
+    'prettier'
+  ],
+  extends: [
+    'plugin:inferno/recommended',
+    'prettier',
+  ],
+  settings: {
+    "inferno": {
+      "createClass": "createClass", // Regex for Component Factory to use,  default to "createClass"
+      "pragma": "Inferno"  // Pragma to use, default to "Inferno"
+    },
+    "propWrapperFunctions": [
+        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+        "forbidExtraProps",
+        {"property": "freeze", "object": "Object"},
+        {"property": "myFavoriteWrapper"}
+    ],
+    "linkComponents": [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      "Hyperlink",
+      {"name": "Link", "linkAttribute": "to"}
+    ]
+  },
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      semi: false,
+      bracketSpacing: true
+    }]
+  },
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      plugins: [
+        '@typescript-eslint',
+      ],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+      ],
+      rules: {
+        
+      },
+    }
+  ]
+}
