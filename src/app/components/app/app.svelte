421e9c99f937ebc13da27faa294f8f845aacb3b5
Daniil Veremchuk <daniel@verydanny.com>
2020-04-23 23:42:40 -0400

    dingoid

--- src/app/components/app/app.svelte
+++ src/app/components/app/app.svelte
@@ -1,10 +1,22 @@
-<h1 class="someClass">Future Sites</h1>
-<script lang="typescript">
-  const dingus: string = 'hello'
-  const someBool: boolean = false
-</script>
 <style src="text/scss">
-@import './normalize.scss';
 @import './app.scss';
 
+.someOther {
+  font-size: 72px;
+  font-family: 'Helvetica Neue', Helvetica, sans-serif;
+  font-weight: 200;
+}
 </style>
+
+<script lang="typescript">
+let count = 0;
+
+function handleClick(event: Event) {
+  count += 1;
+}
+</script>
+
+<h1 class="someOther">Future Site</h1>
+<button on:click={handleClick}>
+	count: {count}
+</button>
